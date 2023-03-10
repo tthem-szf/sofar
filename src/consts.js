24fa6598be85ef238953d2fb6759b1844bad9337
tthem-szf <424149319@qq.com>
2023-03-10 16:12:21 +0800

    更改域名

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Sofar's Blog`;
 export const SITE_DESCRIPTION = '分享我的所见、所学、所经历';
 export const SITE_EMAIL = '424149319@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = 'sofar.vercel.app/';
+export const SITE_URL = "https://sofar.vercel.app/";
