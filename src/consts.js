06140a09026c6dad21ecf60c2f1983cfd033faf1
tthem-szf <424149319@qq.com>
2023-03-10 15:29:15 +0800

    更改了博客标题、描述和邮箱

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_TITLE = `Sofar's Blog`;
+export const SITE_DESCRIPTION = '分享我的所见、所学、所经历';
+export const SITE_EMAIL = '424149319@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
